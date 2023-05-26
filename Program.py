de28e56778dc296f77d5a9cb4edb89b9c11de665
Om Rajaram Wagh <123386026+Waghom25@users.noreply.github.com>
2023-05-26 09:31:07 +0530

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,27 +1,33 @@
-public class Main{
-    public static void main(int key){
-        int n = 7;
-        List<Integer> arr = new ArrayList<Integer>();
-        arr.add(1);
-        arr.add(3);
-        arr.add(6);
-        arr.add(7);
-        arr.add(9);
-        arr.add(10);
-        arr.add(12);
-        bool found = false;
-        int index = -1;
-        for(int i=0;i<n;i++){
-            if(arr[i] == key){
-                found = true;
-                index = i;
-                break;
-            }
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout << "\nEnter the size of the array: ";
+    
+    int n; cin >> n;
+    int arr[n];
+    
+    cout << "\nEnter the array elements in ascending order: ";
+    
+    for(int i=0;i<n;i++) cin >> arr[i];
+    
+    int target;
+    cout << "\nEnter the target element: ";
+    cin >> target;
+    int start = 0, end = n-1, mid, ans = -1;
+    
+    while(start <= end){
+        mid = start + (end-start)/2;
+        if(arr[mid] == target){
+            ans = mid;
+            break;
         }
-        if(found){
-            System.debug(“Element found at ”+ index');
-        }
-        else{
-            System.debug(Element not found');
-        }
+        else if(arr[mid] > target) end = mid - 1;
+        else start = mid + 1;
+    }
+    if(ans!=-1){
+        cout << "\nTarget element is present at "<<ans<<" index in array\n";
+    }
+    else cout << "\nTarget element is not present in the array\n";
+return 0;
 }
